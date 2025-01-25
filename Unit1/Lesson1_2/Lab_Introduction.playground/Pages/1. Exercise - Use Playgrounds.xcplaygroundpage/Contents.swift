53b864febd34fb74a3b50ce9c91f7d12115784b2
NotMDavid <dvdromerojr@gmail.com>
2025-01-25 15:35:52 -0500

    Homework 1
    Woah

--- Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,13 +10,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Everyting But Ordinary - Avril Lavgine")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
+print("Is it enough to die?")
+print("Somebody save my life.")
+print("I rather be anything but ordinary please.")
 
 /*:
 
