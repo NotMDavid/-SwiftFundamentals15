33324d2e706f00866979869d73d5927ebfdd70f0
NotMDavid <dvdromerojr@gmail.com>
2025-02-16 12:09:30 -0500

    homework

--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
@@ -3,11 +3,16 @@
 
  Write a function called `greeting` that takes a `String` argument called name, and returns a `String` that greets the name that was passed into the function. I.e. if you pass in "Sophie" the return value might be "Hi, Sophie! How are you?" Use the function and print the result.
  */
+func greeting(name: String) -> String {
+    return "Hi \(name). How are you?"
+}
 
-
+greeting(name: "David")
 //:  Write a function that takes two `Int` arguments, and returns an `Int`. The function should multiply the two arguments, add 2, then return the result. Use the function and print the result.
-
-
+func twoInt(num1: Int, num2: Int) -> Int {
+    return (num1 * num2) + 2
+}
+twoInt(num1: 29, num2: 5)
 /*:
 [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Separating Functions](@next)
  */
