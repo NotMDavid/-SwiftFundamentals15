464a505c97c637c713157bf56e70d15f3c23c29c
NotMDavid <dvdromerojr@gmail.com>
2025-02-22 12:12:37 -0500

    homework

--- Unit2/Lesson2_5/Lab_Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_5/Lab_Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
@@ -4,9 +4,14 @@
  - Note: The exercises below are based on a game where a spaceship avoids obstacles in space. The ship is positioned at the bottom of a coordinate system and can only move left and right while obstacles "fall" from top to bottom. The base class `Spaceship` and subclasses `Fighter` and `ShieldedShip` have been provided for you below. You will use these to complete the exercises.
  */
 class Spaceship {
-    let name: String
-    var health: Int
-    var position: Int
+    let name: String = ""
+    var health: Int = 0
+    var position: Int = 0
+    
+    init(name: String, health: Int, position: Int) {
+        self.health = health
+        self.position = position
+    }
 
     func moveLeft() {
         position -= 1
@@ -28,6 +33,11 @@ class Fighter: Spaceship {
     let weapon: String
     var remainingFirePower: Int
 
+    init(name: String, health: Int, position: Int, weapon: String, remainingFirePower: Int) {
+        self.remainingFirePower = remainingFirePower
+        self.weapon = weapon
+        super.init(name: name, health: health, position: position)
+    }
     func fire() {
         if remainingFirePower > 0 {
             remainingFirePower -= 1
@@ -39,6 +49,10 @@ class Fighter: Spaceship {
 
 class ShieldedShip: Fighter {
     var shieldStrength: Int
+    init(name: String, health: Int, position: Int, weapon: String, remainingFirePower: Int, shieldStrength: Int) {
+        self.shieldStrength = shieldStrength
+        super.init(name: name, health: health, position: position, weapon: weapon, remainingFirePower: remainingFirePower)
+        }
 
     override func wasHit() {
         if shieldStrength > 0 {
@@ -53,24 +67,28 @@ class ShieldedShip: Fighter {
 
  Then create an instance of `Spaceship` below called `falcon`. Use the memberwise initializer you just created. The ship's name should be "Falcon."
  */
-
-
+let falcon = Spaceship(name: "Falcon", health: 80, position: 0)
 /*:
  Writing initializers for subclasses can get tricky. Your initializer needs to not only set the properties declared on the subclass, but also set all of the uninitialized properties on classes that it inherits from. Go to the declaration of `Fighter` and write an initializer that takes an argument for each property on `Fighter` and for each property on `Spaceship`. Set the properties accordingly. (Hint: you can call through to a superclass's initializer with `super.init` *after* you initialize all of the properties on the subclass).
 
  Then create an instance of `Fighter` below called `destroyer`. Use the memberwise initializer you just created. The ship's name should be "Destroyer."
  */
-
+let Destroyer = Fighter(name: "Destroyer", health: 90, position: 0, weapon: "Gun", remainingFirePower: 10)
 
 /*:
  Now go add an initializer to `ShieldedShip` that takes an argument for each property on `ShieldedShip`, `Fighter`, and `Spaceship`, and sets the properties accordingly. Remember that you can call through to the initializer on `Fighter` using `super.init`.
 
  Then create an instance of `ShieldedShip` below called `defender`. Use the memberwise initializer you just created. The ship's name should be "Defender."
  */
+let defender = ShieldedShip(name: "Defender", health: 100, position: 0, weapon: "Gun", remainingFirePower: 25, shieldStrength: 50)
 
 
-//:  Create a new constant named `sameShip` and set it equal to `falcon`. Print out the position of `sameShip` and `falcon`, then call `moveLeft()` on `sameShip` and print out the position of `sameShip` and `falcon` again. Did both positions change? Why? If both were structs instead of classes, would it be the same? Why or why not? Provide your answer in a comment or print statement below.
-
+let sameShip = falcon
+print("falcon distance = \(falcon.position)")
+print("sameShip distance = \(sameShip.position)")
+sameShip.moveLeft()
+print("falcon distance = \(falcon.position)")
+print("sameShip distance = \(sameShip.position)")
 
 /*:
  _Copyright © 2023 Apple Inc._
